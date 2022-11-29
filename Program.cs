bfbbab6e6653349ac96a541c44f16e4fb5c01fca
Sergey <sergey_ageev13@mail.ru>
2022-11-30 02:00:40 +0300

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+string[] a = CreateArray();
+Console.WriteLine();
+Console.WriteLine(string.Join(", ", a));
+string[] b = SortedArray(a);
+Console.WriteLine(string.Join(", ", b));
+
+
+
+string[] CreateArray()
+{
+    Console.WriteLine("Введите длину масива строк");
+    string[] newArray = new string[LengthArray()];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        Console.WriteLine($"Введите {i}-й элемент массива");
+        newArray[i] = Console.ReadLine();
+        Console.WriteLine();
+    }
+    return newArray;
+
+    int LengthArray()
+    {
+        int intLengthArray;
+        bool check = int.TryParse(Console.ReadLine(), out intLengthArray);
+        if (check && intLengthArray > 0) return intLengthArray;
+        else
+        {
+            Console.WriteLine("Ошибка! Введите положительное чило больше 0");
+            return LengthArray();
+        }
+    }
+}
+
+string[] SortedArray(string[] incomingArray)
+{
+    int cul = 0;
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        if (incomingArray[i].Length < 4) cul++;
+    }
+    string[] newArray = new string[cul];
+    cul = 0;
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        if (incomingArray[i].Length < 4)
+        {
+            newArray[cul] = incomingArray[i];
+            cul++;
+        }
+    }
+    return newArray;
+}
